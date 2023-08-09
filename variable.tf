b7e10a6cd5d3f8c02eebb52cbbf54ce9790f281f
yugandhardevops <134033216+yugandhardevops@users.noreply.github.com>
2023-08-09 20:21:50 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-        region          ="us-east-1"
-        access_key              ="AKIAZWQNTFDD6Y7DS64D"
-        secret_key              ="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"
+        region          ="ap-south-1"
+        access_key              ="AKIAUKWEVKEAXN4KIVHP"
+        secret_key              ="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"
         }
 variable "ami" {
   default = "ami-0763cf792771fe1bd"
